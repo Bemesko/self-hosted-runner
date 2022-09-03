9e4d9c16f7bd4daadcd13b194ce0f77bcf6e4262
Bernardo Mesko <bemesko@gmail.com>
2022-09-03 12:37:31 -0300

    Add build docker image function
    This function receives the Runner token as a parameter and uses it
    to build a new runner image.

--- setup.sh
+++ setup.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
-
 err() {
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
 }
 
-main() {
+get_runner_token(){
   local github_user="$1"
   local github_repo="$2"
   local github_token="$3"
   local json_file
   local runner_token
-
-  if [[ -z $github_user || -z $github_repo || -z $github_token ]]; then
-    err "Wrong number of arguments."
-    err "Usage: setup.sh GITHUB_USER GITHUB_REPO GITHUB_TOKEN"
-    exit 1
-  fi
 
   json_file=$(mktemp)
 
@@ -35,4 +28,35 @@ main() {
   echo $runner_token
 }
 
+build_runner_image() {
+  local github_user="$1"
+  local github_repo="$2"
+  local repo_url="https://github.com/${github_user}/${github_repo}"
+
+  docker build \
+    --tag "gh-runner:${github_repo}" \
+    --build-arg GITHUB_REPO=${repo_url} \
+    --secret id=GITHUB_TOKEN,src=$(pwd)/secrets.txt \
+    .
+
+  echo "Built image gh-runner:${github_repo}"
+}
+
+main() {
+  local github_user="$1"
+  local github_repo="$2"
+  local github_token="$3"
+  local runner_token
+
+  if [[ -z $github_user || -z $github_repo || -z $github_token ]]; then
+    err "Wrong number of arguments."
+    err "Usage: setup.sh GITHUB_USER GITHUB_REPO GITHUB_TOKEN"
+    exit 1
+  fi
+
+  get_runner_token $github_user $github_repo $github_token > secrets.txt
+  build_runner_image $github_user $github_repo
+}
+
+set -e
 main "$@"
